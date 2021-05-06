386110de4ecc554d64663534451e0b9bf52d92e7
hilalfnisanci <hilalfnisanci@gmail.com>
2021-05-06 15:34:56 +0300

    first version

---
+++ src/sample/DatabaseConnection.java
@@ -0,0 +1,24 @@
+package sample;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DatabaseConnection {
+    public Connection databaseLink;
+
+    public Connection getConnection() {
+        String databaseName = "demo_db";
+        String databaseUser = "projectUser";
+        String databasePassword = "User@1234";
+        String url = "jdbc:mysql://localhost/" + databaseName;
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            databaseLink = DriverManager.getConnection(url,databaseUser,databasePassword);
+        } catch (Exception e) {
+            e.printStackTrace();
+            e.getCause();
+        }
+        return databaseLink;
+    }
+}
